c5137f5c5221fa5800243e6efc8aa37d28033d69
giliirox <5869245@gmail.com>
2022-03-25 01:13:31 +0200

    two

--- bytesHW/bytesHW.cpp
+++ bytesHW/bytesHW.cpp
@@ -4,18 +4,18 @@
 
 #include "pch.h"
 #include <iostream>
-//תרגיל 23
+//Question 23
 int CircularLeftShift(int n, int k)
 {
 	
 	return n << k | n >> (32 - k);
 }
-//תרגיל 24
+//Question 24
 int Xor(int a, int b)
 {
 	return (a | b) - (a & b);
 }
-//תרגיל 20
+//Question 20
 int Swap(int a)
 {
 	return a ^ 1;
@@ -24,5 +24,5 @@ int main()
 {
 	/*std::cout << CircularLeftShift(1, 8);*/
 	/*std::cout << Xor(1, 5);*/
-	std::cout << Swap(5);
+	/*std::cout << Swap(5);*/
 }
